1a576123b360c2fdadaf57075c8ab9c3599a704c
your_github_username <your_github_email>
2022-02-21 18:59:35 +0530

    welcome message

---
+++ src/main/java/com/bridgelabz/Day30/InvoiceService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Day30;
+
+public class InvoiceService {
+    public static void main(String[] args) {
+        System.out.println("welcome to cab invoice generation ");
+    }
+}
